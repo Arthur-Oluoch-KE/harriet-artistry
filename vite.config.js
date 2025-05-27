12451ca1399f0a14a737d9be71b8dfb8d537095c
Arthur-Oluoch-KE <arthuroluoch624@gmail.com>
2025-05-27 10:23:04 +0300

    New updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
-   import purgecss from 'vite-plugin-purgecss'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     plugins: [react()],
-     base: '/hearty-fashion/'
-   })
+export default defineConfig({
+  plugins: [react()],
+  base: '/harriet-artistry/'
+})
