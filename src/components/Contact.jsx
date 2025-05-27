62c8acbf14833964d6c39dded904945336dea7fc
Arthur-Oluoch-KE <arthuroluoch624@gmail.com>
2025-05-27 10:48:33 +0300

    Updated social links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,13 @@ function Contact() {
           Reach out at <a href="mailto:oscararthur768@gmail.com" className="contact-link">oluochlynnette15@gmail.com</a> or connect with us on social media!
         </p>
         <div className="social-media">
-          <a href="https://www.facebook.com/0409lynnette?rdid=tmQa7YIt63oYuEAe&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1Y3bYubKRj%2F#" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a href="https://www.facebook.com/share/1Y3bYubKRj/" target="_blank" rel="noopener noreferrer" className="social-link">
             <i className="fab fa-facebook-f"></i>
           </a>
           <a href="https://www.tiktok.com/@0409lynnette?_t=ZM-8wfk5PBqVks&_r=1" target="_blank" rel="noopener noreferrer" className="social-link">
             <i className="fab fa-tiktok"></i>
           </a>
-          <a href="https://www.instagram.com/harrietartistry?igsh=MWt0c3ZuNmdiemg5YQ%3D%3D" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a href="https://www.instagram.com/harrietartistry?igsh=MWt0c3ZuNmdiemg5YQ==" target="_blank" rel="noopener noreferrer" className="social-link">
             <i className="fab fa-instagram"></i>
           </a>
         </div>
